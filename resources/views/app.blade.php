a3650ab4946544bf36cce9ee134fcb36100f97a3
Sourav R <developer.srv1@gmail.com>
2021-04-18 17:42:47 +0530

    alpine & stacks

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     @livewireStyles
+    @stack('styles')
 </head>
 <body>
     @yield('content')
 
     @livewireScripts
+    @stack('scripts')
 </body>
 </html>
