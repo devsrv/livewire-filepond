6c84b0aa567c75358cea687533a0287911ce5fd1
Sourav R <developer.srv1@gmail.com>
2021-04-18 17:30:59 +0530

    livewire serup

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @livewireStyles
+</head>
+<body>
+    @yield('content')
+
+    @livewireScripts
+</body>
+</html>
