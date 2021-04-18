dd251c394e5f2206048e309abe5927b8e266ab83
Sourav R <developer.srv1@gmail.com>
2021-04-18 17:42:42 +0530

    using livewire component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-    <h1>Test</h1>
+    <livewire:update-profile />
 </div>
 @endsection
